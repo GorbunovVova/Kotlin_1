f3f18ad42fb66ce1271559d4c36f23c55c293f49
Gorbunov Vova <vovin@list.ru>
2021-02-22 22:20:44 +0300

    + task 2

---
+++ src/Task_2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+//    Создайте две переменные типа String, содержащие "hello" и "aplana"
+    var hello = "hello"
+    var aplana = "aplana"
+//    И выведите их в консоль так что бы получилось: hello aplana
+    println(hello + " " + aplana)
+
+}
